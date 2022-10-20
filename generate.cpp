e9c77ced1d5d1919024dc3cb680d045c6922c6f3
Aaron Alef <aaron.alef@code.berlin>
2022-10-20 10:42:17 +0200

    chore(data): configure data generator for scenario

--- generate.cpp
+++ generate.cpp
@@ -14,21 +14,21 @@ vector <string> const headers     {"protocol", "peers", "duration", "frequency",
 vector <string> const protocols   {"naive", "voting"};
 vector <int>    const peers       { 10 };
 vector <int>    const durations   { 60 };
-vector <int>    const frequencies { 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60 };
+vector <int>    const frequencies { 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70 };
 
 string build_cmd (string const & prgm, string const & protocol, int peer, int duration, int frequency);
 
 int main (int argc, char * argv[]) {
     if (argc < 3) return EXIT_FAILURE;
     string prgm (argv[1]);
-    bool writeHeader = !filesystem::exists (argv[2]);
 
-    ofstream file;
-    file.open (argv[2], ios_base::app);
-    if (writeHeader) {
-        for (auto const & header : headers) file << header << '\t';
-        file << '\n';
-    }
+    auto tsv_name = string (argv[2]) + ".tsv";
+    auto data_name = string (argv[2]) + ".map";
+    ofstream tsv, data;
+    tsv.open (tsv_name, ios_base::app);
+    data.open (data_name, ios_base::app);
+    for (auto const & header : headers) tsv << header << '\t';
+    tsv << '\n';
 
     auto const iterations = argc == 4 ? std::stoi (argv[3]) : DEFAULT_ITERATIONS;
     array <char, 128> buffer;
@@ -36,19 +36,22 @@ int main (int argc, char * argv[]) {
         for (auto peer : peers) {
             for (auto duration : durations) {
                 for (auto frequency : frequencies) {
+                    data << "#" << protocol << '\t' << peer << '\t' << duration << '\t' << frequency << '\n';
                     for (int i = 1; i <= iterations; i++) {
-                        file << protocol << '\t' << peer << '\t' << duration << '\t' << frequency << '\t';
+                        tsv << protocol << '\t' << peer << '\t' << duration << '\t' << frequency << '\t';
                         auto cmd = build_cmd (prgm, protocol, peer, duration, frequency);
                         (std::cout << i << "/" << iterations << ":\t" << cmd << "\t").flush();
                         unique_ptr<FILE, decltype (& pclose)> pipe (popen (cmd.c_str(), "r"), pclose);
                         if (!pipe) throw runtime_error ("popen() failed!");
                         while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
-                            file << buffer.data();
+                            tsv << buffer.data();
+                            data << buffer.data();
                             std::cout << buffer.data();
                         }
                         std::cout.flush();
+                        tsv.flush();
+                        data.flush();
                     }
-                    file.flush();
                 }
             }
         }
